28794cb3860dbead1ad9f747a2da703836e2956e
Rahul <rahuln@clavax.com>
2016-04-16 14:45:10 +0530

    failing test

--- tests/test_first.py
+++ tests/test_first.py
@@ -1,12 +1,11 @@
+"""Testing modules."""
+
+
 def func(x):
-    """
-    add 1 to x
-    """
+    """Add 1 to x."""
     return x + 3
 
 
 def test_answer():
-    """
-    test for 4 == 5
-        """
+    """Test for 4 == 5."""
     assert func(3) == 5
